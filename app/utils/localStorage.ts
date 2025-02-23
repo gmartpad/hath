c8024658aff309cf08f3b894c59b4533c6b2041e
Gabriel_Martins_Padoin <gabriel.padoin@woli.com.br>
2025-02-23 20:28:04 -0300

    [HATH] LOCALSTORAGE_HELPER - feat: creating localStorage util functions

---
+++ app/utils/localStorage.ts
@@ -0,0 +1,12 @@
+export const getLocalStorage = (key: string): string | null => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem(key);
+  }
+  return null;
+};
+
+export const setLocalStorage = (key: string, value: string): void => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem(key, value);
+  }
+}; 
